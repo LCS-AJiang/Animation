cab24903ac2d211c78f551d094820fa8a87c02fa
Audrey Jiang <ajiang@lcs.on.ca>
2021-12-08 11:25:26 -0500

    Text finished

--- Playground.playground/Pages/Music band poster.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/Music band poster.xcplaygroundpage/Contents.swift
@@ -42,8 +42,7 @@ PlaygroundPage.current.liveView = canvas
 
 // Move the origin from the bottom-left corner of the canvas to it's centre point
 
-// Show a grid
-canvas.drawAxes(withScale: true, by: 50, color: .black)
+
 
 /*:
  ## Add your code
@@ -54,12 +53,101 @@ canvas.drawAxes(withScale: true, by: 50, color: .black)
 
  */
 // Begin writing your code below (you can remove the examples shown)
+// Add a Background
+let currentColorA  = Color(hue: 200,
+                          saturation: 20,
+                          brightness: 40,
+                          alpha: 100)
+canvas.fillColor = currentColorA
+canvas.drawRectangle(at: Point(x: 0, y: 0), width: 400, height: 600)
+
+// Draw the under text rainbow
+let redline = Color(hue: 359,
+                    saturation: 100,
+                    brightness: 100,
+                    alpha: 100)
+canvas.lineColor = redline
+canvas.defaultLineWidth = 5
+canvas.drawLine(from: Point(x: 0, y: 45), to: Point(x: 400, y: 45))
+
+let orangeline = Color(hue: 30,
+                    saturation: 100,
+                    brightness: 100,
+                    alpha: 100)
+canvas.lineColor = orangeline
+canvas.defaultLineWidth = 5
+canvas.drawLine(from: Point(x: 0, y: 50), to: Point(x: 400, y: 50))
+
+let yellowline = Color(hue: 50,
+                    saturation: 100,
+                    brightness: 100,
+                    alpha: 100)
+canvas.lineColor = yellowline
+canvas.defaultLineWidth = 5
+canvas.drawLine(from: Point(x: 0, y: 55), to: Point(x: 400, y: 55))
+
+let greenline = Color(hue: 141,
+                    saturation: 100,
+                    brightness: 89,
+                    alpha: 100)
+canvas.lineColor = greenline
+canvas.defaultLineWidth = 5
+canvas.drawLine(from: Point(x: 0, y: 60), to: Point(x: 400, y: 60))
+
+let bluegreenline = Color(hue: 172,
+                    saturation: 100,
+                    brightness: 90,
+                    alpha: 100)
+canvas.lineColor = bluegreenline
+canvas.defaultLineWidth = 5
+canvas.drawLine(from: Point(x: 0, y: 65), to: Point(x: 400, y: 65))
+
+let blueline = Color(hue: 210,
+                    saturation: 100,
+                    brightness: 100,
+                    alpha: 100)
+canvas.lineColor = blueline
+canvas.defaultLineWidth = 5
+canvas.drawLine(from: Point(x: 0, y: 70), to: Point(x: 400, y: 70))
+
+let purpleline = Color(hue: 266,
+                    saturation: 100,
+                    brightness: 100,
+                    alpha: 100)
+canvas.lineColor = purpleline
+canvas.defaultLineWidth = 5
+canvas.drawLine(from: Point(x: 0, y: 75), to: Point(x: 400, y: 75))
+
 // Add text
+let currentColorB = Color .white
+canvas.textColor = currentColorB
+canvas.drawText(message: "WATER · GILMOUR · MASON · WRIGHT", at: Point (x: 13, y: 13), size: 20, kerning : 0)
+canvas.drawText(message: "LIVE ON STAGE", at: Point (x: 8, y: 41), size: 50, kerning : 0)
+canvas.drawText(message: "CARNEGIE HALL", at: Point (x: 68, y: 98), size: 30, kerning : 0)
+canvas.drawText(message: "THE DARK SIDE OF THE MOON TOUR", at: Point(x: 18, y: 568), size: 20, kerning: 0)
+canvas.drawText(message: "PINK FLOYD", at: Point(x: 18, y: 508), size: 60, kerning: 0)
+
 let currentColorC = Color .black
 canvas.textColor = currentColorC
-canvas.drawText(message: "WATER · GILMOUR · MASON · WRIGHT", at: Point (x: 20, y: 550), size: 8, kerning : 0)
-canvas.drawText(message: "august 19 1977", at: Point (x: 20, y: 535), size: 8, kerning : 0)
+canvas.drawText(message: "WATER · GILMOUR · MASON · WRIGHT", at: Point (x: 15, y: 15), size: 20, kerning : 0)
+canvas.drawText(message: "LIVE ON STAGE", at: Point (x: 10, y: 43), size: 50, kerning : 0)
+canvas.drawText(message: "CARNEGIE HALL", at: Point (x: 70, y: 100), size: 30, kerning : 0)
+canvas.drawText(message: "THE DARK SIDE OF THE MOON TOUR", at: Point(x: 20, y: 570), size: 20, kerning: 0)
 
+let currentColorD = Color . red
+canvas.textColor = currentColorD
+canvas.drawText(message: "PINK FLOYD", at: Point(x: 20, y: 510), size: 60, kerning: 0)
+
+//// Draw the loop of circle in the middle
+canvas.fillColor = .white
+for circle in stride(from: 200,
+                     through: 400,
+                     by: 60){
+    canvas.drawEllipse(at: Point(x: 200, y: 320), width: circle, height: circle)
+}
+
+// Show a grid
+canvas.drawAxes(withScale: true, by: 50, color: .black)
 /*:
  ## Show the Live View
  Don't see any results?
